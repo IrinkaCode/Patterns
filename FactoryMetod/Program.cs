c36fc2031ea608ee873e83e941510f3702f38dcb
IrinkaCode <pil02022012@gmail.com>
2025-01-11 00:32:06 +0200

    Добавьте файлы проекта.

---
+++ FactoryMetod/Program.cs
@@ -0,0 +1,36 @@
+﻿IworkShop creator = new CarWorkShop();
+IProduction car = creator.Create();
+creator = new TruckWorkShop();
+IProduction track = creator.Create();
+car.Release();
+track.Release();
+interface IProduction
+{
+    void Release();
+}
+class Car:IProduction
+{
+    public void Release() =>  Console.WriteLine("Car");
+
+
+}
+
+class Truck:IProduction
+{
+    public void Release() =>  Console.WriteLine("Truck");
+
+
+
+}
+interface IworkShop
+{
+    IProduction Create();
+}
+class CarWorkShop : IworkShop
+{
+    public IProduction Create() => new Car();
+}
+class TruckWorkShop : IworkShop
+{
+    public IProduction Create() => new Truck();
+}
