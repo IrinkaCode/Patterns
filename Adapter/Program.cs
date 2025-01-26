b1db368f24e76d572ce704080a0d0edf1c5563e3
IrinkaCode <pil02022012@gmail.com>
2025-01-26 20:23:23 +0200

    24/01

---
+++ Adapter/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите массу: ");
+float kg = float.Parse(Console.ReadLine()!);
+float lb= kg;
+IScales rScales = new RusssianScales(kg);
+IScales bScales = new AdapterForBritishScales(new BritishScales(lb));
+Console.WriteLine(rScales.GetWeight());
+Console.WriteLine(bScales.GetWeight());
+
+interface IScales
+{
+    float GetWeight();
+
+}
+class RusssianScales : IScales
+{
+    private float currentWeght;
+    public RusssianScales(float cw) => currentWeght=cw;
+
+    public float GetWeight() => currentWeght;
+ 
+}
+class BritishScales : IScales
+{
+    private float currenrweight;
+    public BritishScales(float cw) => currenrweight = cw;
+
+    public float GetWeight() => currenrweight;
+
+}
+
+class AdapterForBritishScales : IScales
+{
+    private BritishScales britishScales;
+    public AdapterForBritishScales(BritishScales britishScales)
+    {
+        this.britishScales = britishScales;
+    }
+    public float GetWeight() => britishScales.GetWeight() * 0.456f;
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Channels;
+
+Sender sender = new EMailSender(new DataBaseReader());
+sender.Send();
+sender.SetDataReader(new FileReader());
+sender.Send();
+sender = new TelegramBotSender(new DataBaseReader());
+sender.Send();
+
+interface IDataReader
+{
+    void Read();
+
+}
+class DataBaseReader : IDataReader
+{
+    public void Read() => Console.WriteLine("Данные с БД");
+
+}
+class FileReader : IDataReader
+{
+    public void Read() => Console.WriteLine("Данные с файла");
+}
+
+abstract class Sender
+{
+    protected IDataReader reader;
+
+    protected Sender(IDataReader reader) => this.reader = reader;
+    public void SetDataReader(IDataReader _reader)=>reader = _reader;
+    public abstract void Send();
+
+}
+class EMailSender : Sender
+{
+    public EMailSender(IDataReader reader) : base(reader) { }
+    public override void Send()
+    {
+       reader.Read();
+        Console.WriteLine("send by email");
+    }
+}
+class TelegramBotSender : Sender
+{
+    public TelegramBotSender(IDataReader reader): base(reader) { }
+    public override void Send()
+    {
+        reader.Read();
+        Console.WriteLine("send by telegram bot");
+    }
+}
